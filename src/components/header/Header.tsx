e9c72a1890b5acc41ce16b77ecfcde6d5d4674f1
Danielle1Melo <danielle.melo@estudante.ifro.edu.br>
2024-07-04 11:56:17 -0400

    inicializando header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,19 @@
-export function Header(){
-    <div>
-        Hello
+"use client";
+import styled from "../../style/allStyles/homePage.module.css"
+import { LOGO } from "@/utils/logos";
+import Image from "next/image";
+
+export function HeaderPage() {
+  return (
+    <div className={styled.container}>
+      <div>
+        <Image src={LOGO.MainLogo} alt="" width={60} height={60} />
+      </div>
+      <div className={styled.links}>
+        <button>About</button>
+        <button>Work</button>
+        <button>Contact</button>
+      </div>
     </div>
+  );
 }
